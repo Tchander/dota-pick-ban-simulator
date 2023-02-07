d0afac66792cbc17e418e6049c8d7992491aa5e9
Tchander <evgeniy.tchander@yandex.ru>
2023-02-07 15:09:49 +0300

    feat: Added heroes info and images

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 
+import './assets/global.css';
+
 const pinia = createPinia();
 const app = createApp(App);
 
